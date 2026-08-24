adb5b847818fbe434cc4fe4e74f583baa555e505
UdiPadovan <udi.p.sousa@gmail.com>
2026-08-24 08:42:40 -0300

    feat: implementa efeito de virar cartas com html, css e js

---
+++ animacao/script.js
@@ -0,0 +1,14 @@
+Script.js
+document.addEventListener('DOMContentLoaded', () => {
+const carta = document.getElementById('carta');
+const btnVirar = document.getElementById('btnVirar');
+const btnDesvirar = document.getElementById('btnDesvirar');
+btnVirar.addEventListener('click', () => {
+carta.classList.remove('animar-desvirar');
+carta.classList.add('animar-virar');
+});
+btnDesvirar.addEventListener('click', () => {
+carta.classList.remove('animar-virar');
+carta.classList.add('animar-desvirar');
+});
+});
